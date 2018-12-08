79637006f8ae5c969242e4bb468af38f44f5949b
vozdp <safihayed@it-simple.net>
2018-12-07 23:12:37 -0500

    All packages are upgraded to the current versions, also jade->pug, static-favicon->serve-favicon, bitcoin-node-api->bitcoin-core. Default settings addopted to ANI

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var express = require('express')
   , path = require('path')
-  , bitcoinapi = require('bitcoin-node-api')
-  , favicon = require('static-favicon')
+  , favicon = require('serve-favicon')
   , logger = require('morgan')
   , cookieParser = require('cookie-parser')
   , bodyParser = require('body-parser')
   , settings = require('./lib/settings')
   , routes = require('./routes/index')
+  , bitcoinapi = require('./lib/api')
   , lib = require('./lib/explorer')
   , db = require('./lib/database')
   , locale = require('./lib/locale')
@@ -39,7 +39,7 @@ if (settings.heavy != true) {
 }
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(favicon(path.join(__dirname, settings.favicon)));
 app.use(logger('dev'));
